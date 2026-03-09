b90207b003b9448a874d23de85c5d235144acdbf
keshak-Ctrl <keshaktilijapun@gmail.com>
2026-03-09 19:00:40 +0000

    js file

---
+++ static/script.js
@@ -0,0 +1,85 @@
+let text = "Dear Jennie... \
+Before this day ends, there is something I want you to know. \
+You are incredibly special and your smile can brighten even the darkest days. \
+Today the world celebrates you. \
+Happy Birthday Jennie 💖";
+
+let i = 0;
+
+function typeWriter(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typeWriter,60);
+
+}
+
+}
+
+function hearts(){
+
+setInterval(function(){
+
+let heart = document.createElement("div");
+
+heart.className="heart";
+
+heart.innerHTML="💗";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.fontSize=(Math.random()*25+15)+"px";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{heart.remove()},6000);
+
+},300)
+
+}
+
+function fireworks(){
+
+setInterval(function(){
+
+let fire = document.createElement("div");
+
+fire.innerHTML="🎆";
+
+fire.style.position="absolute";
+
+fire.style.left=Math.random()*100+"vw";
+
+fire.style.top=Math.random()*100+"vh";
+
+fire.style.fontSize="40px";
+
+document.body.appendChild(fire);
+
+setTimeout(()=>{fire.remove()},2000);
+
+},500)
+
+}
+
+function start(){
+
+document.getElementById("title").innerHTML="Hi Jennie 💖";
+
+typeWriter();
+
+hearts();
+
+setTimeout(function(){
+
+fireworks();
+
+document.getElementById("final").style.display="block";
+
+},8000)
+
+}
